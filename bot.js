67e5e1d3adc23d0111e14576ad60b7855e908604
iovoid <iovoid@users.noreply.github.com>
2014-07-18 14:07:51 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 //Bot for tptapi:
-//login:
+//Login:
 var usnm="username";
 var passw="password";
 var loginurl="http://tptapi.com/login_proc.php?login"+encodeURI("Password:"passw)+encodeURI("Username:"usnm);
 var xhr= New XMLHttpRequest();
 xhr.open(false,loginurl,"GET");
 xhr.send();
+//Print how many money have the user:
+//False will be remplaced by true or a variable or the while may be removed.
 while false{
     var money=function(){
     xhr.open(false,"http://tptapi.com/money.php","GET")
