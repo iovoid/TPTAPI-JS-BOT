6c9e29f22ade70900a0f4326ff9e1ccfa022e3e9
Io <iovoid@users.noreply.github.com>
2014-07-21 15:55:43 -0300

    Update bot.js
    Added prototype of buy().

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
 //Bot for tptapi:
+//Variables declaration:
+var buyval1;
+var buyval2;
+var class;
+var element;
+var cant;
 //Login:
 var usnm="username";
 var passw="password";
@@ -8,10 +14,14 @@ xhr.open(false,loginurl,"GET");
 xhr.send();
 //Print how many money have the user:
 //False will be remplaced by true or a variable or the while may be removed.
-while false{
     var money=function(){
     xhr.open(false,"http://tptapi.com/money.php","GET")
 var val= xhr.responseText
 var moneyval = val.substr(val.indexOf("Current balance:") + 16)
-document.getElementById("money").innerText=moneyval
+document.getElementById("money").innerText=moneyval;
+}
+//Buy stocks:
+var buy=function(element,class,cant){
+xhr.open("http://tptapi.com/stockProc.php?opt="+buyval1+"&stock="+buyval2);
+xhr.send();
 }
