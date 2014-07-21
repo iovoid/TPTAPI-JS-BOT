cfc26bf06b41457bb06abb050e299fe37b2dc979
Io <iovoid@users.noreply.github.com>
2014-07-21 17:40:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,28 @@
-//Bot for tptapi:
+//Framework for tptapi:
 //Variables declaration:
 var buyval1;
 var buyval2;
-var class;
+var sclass;
 var element;
 var cant;
 //Login:
 var usnm="username";
 var passw="password";
-var loginurl="http://tptapi.com/login_proc.php?login"+encodeURI("Password:"+passw)+encodeURI("Username:"+usnm);
+var loginurl="http://tptapi.com/login_proc.php?login";
 var xhr= New XMLHttpRequest();
-xhr.open(false,loginurl,"GET");
-xhr.send();
+xhr.open("POST",loginurl,true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.send("Username="+usnm+"&Password="+passw);
 //Print how many money have the user:
 //False will be remplaced by true or a variable or the while may be removed.
     var money=function(){
-    xhr.open(false,"http://tptapi.com/money.php","GET")
-var val= xhr.responseText
-var moneyval = val.substr(val.indexOf("Current balance:") + 16)
+    xhr.open(false,"http://tptapi.com/money.php","GET");
+var val= xhr.responseText;
+var moneyval = val.substr(val.indexOf("Current balance:") + 16);
 document.getElementById("money").innerText=moneyval;
 }
 //Buy stocks:
-var buy=function(element,class,cant){
+var buy=function(element,sclass,cant){
 xhr.open("http://tptapi.com/stockProc.php?opt="+buyval1+"&stock="+buyval2);
 xhr.send();
 }
